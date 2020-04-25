d3e8ecae5e3f5a7496792378819bb2c6aa5f91bf
Максим Алексеев <mrvalter566@gmail.com>
2020-04-25 12:20:40 +0700

    Add functionality to get all files from directory and read text

--- main.go
+++ main.go
@@ -1,5 +1,29 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"io/ioutil"
+)
 
+func main() {
+	files, err := ioutil.ReadDir("./")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	for _, f := range files {
+		if f.Name() == ".git" {
+			continue
+		}
+		fmt.Println(f.Name())
+
+		text, err := ioutil.ReadFile(f.Name())
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		fmt.Println(string(text))
+	}
 }
